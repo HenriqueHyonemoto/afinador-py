29a407828ee8a822db9482897fe5792312805ccd
HenriqueHyonemoto <ifhenriquehyonemoto@gmail.com>
2024-12-04 21:54:01 -0300

    ajuster

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# detector-notas-musicais
+# Afinação Padrão
+
+E<BR>
+B<BR>
+G<BR>
+D<BR>
+A<BR>
+E<BR>
+
+# Half Step Down (1/2)
+
+D#<BR>
+A#<BR>
+F#<BR>
+C#<BR>
+G#<BR>
+D#<BR>
--- afinador.py
+++ afinador.py
@@ -5,33 +5,33 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 # Configurações do áudio
-FORMAT = pyaudio.paInt16  # Formato de áudio
-CHANNELS = 1  # Número de canais
-RATE = 44100  # Taxa de amostragem
-CHUNK = 1024  # Tamanho do buffer
+FORMATO = pyaudio.paInt16  # Formato de áudio
+CANAIS = 1  # Número de canais
+TAXA_AMOSTRAGEM = 44100  # Taxa de amostragem
+TAMANHO_BUFFER = 1024  # Tamanho do buffer
 
-VOLUME_THRESHOLD = 300  # MODIFICAVEL | serve para definir volume minimo de audio detectavel
-MARGEM_ERRO = 0.5  # MODIFICAVEL | Margem de erro para a afinação
+LIMIAR_VOLUME = 150  # MODIFICAVEL | serve para definir volume minimo de audio detectavel
+MARGEM_ERRO = 1  # MODIFICAVEL | Margem de erro para a afinação
 
 # Variáveis globais para armazenar a última nota, instrução e frequência detectadas
-last_note = None
-last_instruction = None
-last_frequency = None
+ultima_nota = None
+ultima_instrucao = None
+ultima_frequencia = None
 
 # Função para capturar o áudio
-def capture_audio():
+def capturar_audio():
     audio = pyaudio.PyAudio()
 
-    stream = audio.open(format=FORMAT,
-                        channels=CHANNELS,
-                        rate=RATE,
+    stream = audio.open(format=FORMATO,
+                        channels=CANAIS,
+                        rate=TAXA_AMOSTRAGEM,
                         input=True,
-                        frames_per_buffer=CHUNK)
+                        frames_per_buffer=TAMANHO_BUFFER)
 
     frames = []
 
-    for i in range(0, int(RATE / CHUNK * 1)):  # MODIFICAVEL | grava audio de 1 em 1 segundo
-        data = stream.read(CHUNK)
+    for i in range(0, int(TAXA_AMOSTRAGEM / TAMANHO_BUFFER * 1)):  # MODIFICAVEL | grava audio de 1 em 1 segundo
+        data = stream.read(TAMANHO_BUFFER)
         frames.append(data)
 
     stream.stop_stream()
@@ -41,17 +41,17 @@ def capture_audio():
     return b''.join(frames)
 
 # Função para processar o áudio e encontrar a frequência dominante
-def process_audio(audio_data):
-    audio_array = np.frombuffer(audio_data, dtype=np.int16)
-    N = len(audio_array)
-    T = 1.0 / RATE
-    yf = scipy.fft.fft(audio_array)
+def processar_audio(dados_audio):
+    array_audio = np.frombuffer(dados_audio, dtype=np.int16)
+    N = len(array_audio)
+    T = 1.0 / TAXA_AMOSTRAGEM
+    yf = scipy.fft.fft(array_audio)
     xf = scipy.fft.fftfreq(N, T)[:N//2]
 
     # Calcular a amplitude média
-    amplitude_mean = np.mean(np.abs(audio_array))
+    amplitude_media = np.mean(np.abs(array_audio))
 
-    if amplitude_mean < VOLUME_THRESHOLD:
+    if amplitude_media < LIMIAR_VOLUME:
         return None, xf, np.abs(yf[:N//2])  # Ignorar se a amplitude média estiver abaixo do limiar
 
     # Encontrar a frequência dominante
@@ -61,48 +61,56 @@ def process_audio(audio_data):
     return freq, xf, np.abs(yf[:N//2])
 
 # Função para mapear a frequência para a nota musical
-def frequency_to_note(freq):
-    notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+def frequencia_para_nota(freq):
+    notas = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
     A4 = 440.0
     C0 = A4 * pow(2, -4.75)
-    name = ""
+    nome = ""
 
     if freq == 0:
-        return "N/A"
+        return None
 
     h = round(12 * np.log2(freq / C0))
-    octave = h // 12
+    oitava = h // 12
     n = h % 12
-    name = notes[n] + str(octave)
 
-    return name, h
+    if n < 0 or n >= len(notas):
+        return None
+
+    nome = notas[n] + str(oitava)
+
+    return nome, h
 
 # Função para determinar se a corda deve ser apertada ou afrouxada
-def tuning_instructions(freq, note, note_frequency):
-    difference = freq - note_frequency
-    if abs(difference) <= MARGEM_ERRO:
+def instrucoes_afinacao(freq, nota, frequencia_nota):
+    diferenca = freq - frequencia_nota
+    if abs(diferenca) <= MARGEM_ERRO:
         return "Afinado"
-    elif difference > MARGEM_ERRO:
+    elif diferenca > MARGEM_ERRO:
         return "Afrouxe a corda"
-    elif difference < -MARGEM_ERRO:
+    elif diferenca < -MARGEM_ERRO:
         return "Aperte a corda"
 
 # Função para converter nota para frequência
-def note_to_frequency(note):
-    notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+def nota_para_frequencia(nota):
+    notas = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
     A4 = 440.0
     C0 = A4 * pow(2, -4.75)
-    note_name = note[:-1]
-    octave = int(note[-1])
-    note_index = notes.index(note_name)
-    h = 12 * octave + note_index
+    nome_nota = nota[:-1]
+    oitava = int(nota[-1])
+
+    if nome_nota not in notas:
+        return None
+
+    indice_nota = notas.index(nome_nota)
+    h = 12 * oitava + indice_nota
     return C0 * pow(2, h / 12)
 
 # Função para atualizar o gráfico
-def update_plot(frame):
-    global last_note, last_instruction, last_frequency
-    audio_data = capture_audio()
-    freq, xf, yf = process_audio(audio_data)
+def atualizar_grafico(frame):
+    global ultima_nota, ultima_instrucao, ultima_frequencia
+    dados_audio = capturar_audio()
+    freq, xf, yf = processar_audio(dados_audio)
 
     plt.clf()
     plt.plot(xf, yf)
@@ -110,29 +118,46 @@ def update_plot(frame):
     plt.ylabel('Magnitude')
 
     if freq is not None:
-        note, h = frequency_to_note(freq)
-        note_frequency = note_to_frequency(note)
-        last_note = f"{note} ({note_frequency:.2f} Hz)"
-        instruction = tuning_instructions(freq, note, note_frequency)
-        last_instruction = instruction
-        last_frequency = freq
+        info_nota = frequencia_para_nota(freq)
+        if info_nota is None:
+            # Se a nota não for reconhecida, continuar mostrando a última nota reconhecida
+            if ultima_nota:
+                plt.title(f'Última Frequência: {ultima_frequencia:.2f} Hz\nÚltima Nota: {ultima_nota}\nÚltima Instrução: {ultima_instrucao}')
+            else:
+                plt.title('Nota não reconhecida')
+            return
+
+        nota, h = info_nota
+        frequencia_nota = nota_para_frequencia(nota)
+        if frequencia_nota is None:
+            # Se a nota não for reconhecida, continuar mostrando a última nota reconhecida
+            if ultima_nota:
+                plt.title(f'Última Frequência: {ultima_frequencia:.2f} Hz\nÚltima Nota: {ultima_nota}\nÚltima Instrução: {ultima_instrucao}')
+            else:
+                plt.title('Nota não reconhecida')
+            return
+
+        ultima_nota = f"{nota} ({frequencia_nota:.2f} Hz)"
+        instrucao = instrucoes_afinacao(freq, nota, frequencia_nota)
+        ultima_instrucao = instrucao
+        ultima_frequencia = freq
 
         # Definir a cor do texto com base na instrução
-        if instruction == "Afinado":
-            text_color = 'green'
+        if instrucao == "Afinado":
+            cor_texto = 'green'
         else:
-            text_color = 'orange'
+            cor_texto = 'orange'
 
-        plt.title(f'Frequência Dominante: {freq:.2f} Hz\nNota: {last_note}\nInstrução: {instruction}', color=text_color)
+        plt.title(f'Frequência Dominante: {freq:.2f} Hz\nNota: {ultima_nota}\nInstrução: {instrucao}', color=cor_texto)
     else:
-        if last_note:
-            plt.title(f'Última Frequência: {last_frequency:.2f} Hz\nÚltima Nota: {last_note}\nÚltima Instrução: {last_instruction}')
+        if ultima_nota:
+            plt.title(f'Última Frequência: {ultima_frequencia:.2f} Hz\nÚltima Nota: {ultima_nota}\nÚltima Instrução: {ultima_instrucao}')
         else:
             plt.title('Volume abaixo do limiar')
 
 # Configura a animação do Matplotlib
 fig, ax = plt.subplots()
-ani = FuncAnimation(fig, update_plot, interval=1000)  # MODIFICAVEL, Atualiza o grafico a cada 1 segundo
+ani = FuncAnimation(fig, atualizar_grafico, interval=1000)  # MODIFICAVEL, Atualiza o grafico a cada 1 segundo
 
 # Mostra a janela do Matplotlib
 plt.show()
